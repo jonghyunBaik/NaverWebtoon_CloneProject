d61a245be3c076f95a486096fb4b58de5bacc908
jonghyunBaik <pigmammam4@gmail.com>
2023-11-23 20:44:25 +0900

    [update] podFile error 로 인한 프로젝트 파일 이전

---
+++ naverWebtoonClone/naverWebtoonClone/NaverWebtoonCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloneNaverWebtoonApp.swift
+//  CloneNaverWebtoon
+//
+//  Created by jonghyun baik on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct NaverWebtoonCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
